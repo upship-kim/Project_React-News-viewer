5569bfa7104db668ef1bcf135bc5c9cb402904cf
upship-kim <tkdqook@gmail.com>
2021-03-15 15:15:31 +0900

    UI Success

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react';
+import axios from 'axios';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try{
+      const response = await axios.get(
+        'http://newsapi.org/v2/top-headlines?country=kr&category=entertainment&apiKey=87adf8cd864b4d4e914cb9d92302b635',
+      );
+      setData(response.data);
+      } catch(e){
+        console.log(e);
+      }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Categories/>
+      <NewsList/>
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
---
+++ src/components/NewsList.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components';
+import axios from 'axios';
+import NewsItem from './NewsItem'
+
+
+const NewsListBlock = styled.div`
+    box-sizing: border-box;
+    padding-bottom: 3rem;
+    width: 768px;
+    margin: 0 auto;
+    margin-top: 2rem;
+    @media screen and (max-width: 768px){
+        width: 100%;
+        padding-left: 1rem;
+        padding-right: 1rem;
+    } 
+`;
+
+
+
+const NewsList = () => {
+    const [articles, setArticles] = useState(null); 
+    const [loading, setLoading] = useState(false);
+
+    useEffect( () => {
+        //async를 사용하는 함수 따로 선언
+        const fetchData = async () => {
+            setLoading(true); 
+            try {
+                const response = await axios.get(
+                    'http://newsapi.org/v2/top-headlines?country=kr&category=entertainment&apiKey=87adf8cd864b4d4e914cb9d92302b635',
+                );
+                setArticles(response.data.articles);
+                
+            } catch (e) {
+                console.log(e);
+            } setLoading(false);
+        };
+        fetchData();    //초기 렌더링 
+    }, []);
+    
+    //대기중일때 
+    if(loading) {
+        return <NewsListBlock>대기중...</NewsListBlock>
+    }
+
+    //아직 articles 값이 설정되지 않았을 때 
+    if(!articles) {
+        return null;
+    }
+
+    //ariticles 값이 유효 할때
+    return (
+        <NewsListBlock>
+            {articles.map(article => (
+                <NewsItem key={article.url} article={article}/>
+            ))}
+        </NewsListBlock>
+    )
+}
+
+export default NewsList
